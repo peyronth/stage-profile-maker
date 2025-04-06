8e60d138d72e4ce47ac4aa2ff0423345d6ee053e
peyronth <peyron.th@gmail.com>
2025-04-06 19:35:50 +0200

    Fix github page path

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/stage-profile-maker/',
   optimizeDeps: {
     include: ['vuetify', '@vuetify/labs'],
     exclude: ['vuetify/labs'],
