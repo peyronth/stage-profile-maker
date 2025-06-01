d8b10ea629056ac5252b2776c554eabbd319230c
peyronth <peyron.th@gmail.com>
2025-06-01 18:46:22 +0200

    Add multi format export

--- example/src/vite-env.d.ts
+++ example/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module 'dom-to-image-more' {
+  interface DomToImage {
+    toSvg(node: HTMLElement, options?: any): Promise<string>;
+    toPng(node: HTMLElement, options?: any): Promise<string>;
+    toJpeg(node: HTMLElement, options?: any): Promise<string>;
+    toBlob(node: HTMLElement, options?: any): Promise<Blob>;
+  }
+  const domtoimage: DomToImage;
+  export default domtoimage;
+}
